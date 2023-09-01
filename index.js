18f223997a16beeb90c10ee3eff26f3f79aca773
AbroShahzeb <khizr772@gmail.com>
2023-09-01 03:53:33 -0700

    Added database

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-
-async function connectDB() {
-  const connection = await mongoose.connect(process.env.MONGO_URI);
-  console.log("Connected!");
-  return connection;
-}
-
-let db = connectDB();
-console.log(db);
-
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
+
+// Use the MongoDB URI from the environment variable
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  // Add other fields as needed
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/post-article", async (req, res) => {
+  const article = new Article({
+    title: "Article title",
+    content: "Article content",
+  });
+  await article.save();
+  res.json({ message: "Article inserted" });
+});
+
+app.get("/articles", async (req, res) => {
+  try {
+    const articles = await Article.find();
+    res.json(articles);
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    res.status(500).json({ error: "Unable to fetch articles" });
+  }
+});
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.get("/", async (req, res) => {
+  const data = await db.collection("articles").find();
   console.log("Just got a request!");
-  res.json({ message: "Homepage!" });
+  res.json(data);
 });
 
 app.get("/user", (req, res) => {
