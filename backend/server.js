e05552850a9477b7eafa5b4a1a64f696da27b3f8
AbroShahzeb <khizr772@gmail.com>
2023-09-21 18:29:06 +0500

    Added frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import path from "path";
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    console.log("Some changes :)");
+    res.send("API is running....");
+  });
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+function Navigation() {
+  return (
+    <nav className='p'>
+      <div>Logo</div>
+      <ul>
+        <li>About</li>
+        <li>Products</li>
+        <li>Contact</li>
+        <li>Account</li>
+      </ul>
+      <div>Avatar</div>
+    </nav>
+  );
+}
+
+export default Navigation;
