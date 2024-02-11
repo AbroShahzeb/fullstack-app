f70f10faf8270b3e31140d71fda22370dc3049f8
AbroShahzeb <khizr772@gmail.com>
2024-02-11 20:40:37 +0500

    Update supabase.js

--- backend/utils/supabase.js
+++ backend/utils/supabase.js
@@ -1,11 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAPI = process.env.SUPABASE_API;
+console.log(supabaseUrl, supabaseAPI);
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_API
-);
+const supabase = createClient(supabaseUrl, supabaseAPI);
 
 export default supabase;
