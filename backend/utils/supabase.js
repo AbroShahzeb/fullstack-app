493e634bc582f387a0ab42de0bf1eb750e734d02
AbroShahzeb <khizr772@gmail.com>
2024-02-11 20:32:19 +0500

    Update supabase.js

--- backend/utils/supabase.js
+++ backend/utils/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
