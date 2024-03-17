85cd97a2ba737f5c05151b69042ae86338054446
AbroShahzeb <khizr772@gmail.com>
2024-03-17 15:39:43 +0500

    Rebuilt the frontend

--- frontend/src/app/apiSlice.js
+++ frontend/src/app/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "https://quizzy.cyclic.sh" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://quizzy.cyclic.app" });
 
 export const apiSlice = createApi({
   baseQuery,
